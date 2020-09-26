da3cfd134771502d6c18081b2bafb5959895e860
Adigun Sherif <adigunsherif@gmail.com>
2020-09-26 07:24:02 -0500

    updates

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rashaad_acnt.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'school_account.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
