e35fd0c86eb95b694165c5bae1096188112a2e99
Adigun Sherif <adigunsherif@gmail.com>
2020-09-26 10:23:27 -0500

    updates

--- school_account/settings.py
+++ school_account/settings.py
@@ -26,7 +26,7 @@ if os.path.isfile(dotenv_file):
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '%$q99rs=d9pfvu_-&gcuz!znzdtb6$n)&7&la$%n*hv0)lpb0f'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
